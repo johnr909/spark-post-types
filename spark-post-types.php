22b128a034eb6679ad976764d2ecd3c35d5c736a
John Rose <johnr909@yahoo.com>
2021-12-18 09:41:50 -0700

    scaffolded the plugin via wp-cli

---
+++ spark-post-types.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Spark Post Types
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     spark-post-types
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Spark_Post_Types
+ */
+
+// Your code starts here.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Spark_Post_Types
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
