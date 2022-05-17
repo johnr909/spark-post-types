a82cf179cab1e039da71320146bc36f14b5b6367
John Rose <johnr909@yahoo.com>
2022-05-17 09:29:48 -0600

    Altered the daily deal CPT data a little in anticipation of how it will be used

--- dailydeals/custom-fields-dailydeals-form.php
+++ dailydeals/custom-fields-dailydeals-form.php
@@ -1,33 +1,33 @@
-<div class="box">
-    <style scoped>
-        .box {
-            display: grid;
-            grid-template-columns: max-content 1fr;
-            grid-row-gap: 10px;
-            grid-column-gap: 20px;
-        }
-        .field {
-            display: contents;
-        }
-    </style>
+<div class="fields-meta-box">
 <?php
-    $deal_title = get_post_meta($post->ID, 'dealTitle', true);
-    $deal_description = get_post_meta($post->ID, 'dealDescription', true);
+    $deal_title = get_post_meta( $post->ID, 'dealTitle', true );
+    $deal_lead_off = get_post_meta( $post->ID, 'dealLeadOff', true );
+    $deal_description_text = get_post_meta( $post->ID, 'dealDescriptionText', true );
 ?>
-    <p class="field">
-        <label for="dealTitle"><?php _e('Deal Title', 'spark-post-types'); ?></label>
+    <p class="custom-field-control">
+        <label for="dealTitle"><?php _e( 'Deal Title', 'spark-post-types' ); ?></label>
         <input id="dealTitle" 
                type="text" 
                name="dealTitle"
-               value="<?php echo esc_attr($deal_title); ?>">
+               value="<?php echo esc_attr( $deal_title ); ?>">
     </p>
 
-    <p class="field">
-        <label for="dealDescription"><?php _e('Deal Description', 'spark-post-types'); ?></label>
-        <input id="dealDescription"  
-                  name="dealDescription"
-                  type="text"
-                  value="<?php echo esc_attr($deal_description); ?>">
+    <p class="custom-field-control">
+        <label for="dealLeadOff"><?php _e( 'Deal Lead Off', 'spark-post-types' ); ?></label>
+        <input id="dealLeadOff" 
+               type="text" 
+               name="dealLeadOff"
+               value="<?php echo esc_attr( $deal_lead_off ); ?>">
     </p>
 
-</div><!-- /.box -->
+    <p class="custom-field-control">
+        <label for="dealDescriptionText"><?php _e( 'Deal Description', 'spark-post-types' ); ?></label>
+        <textarea id="dealDescriptionText"  
+                  name="dealDescriptionText"
+                  value="<?php echo esc_attr( $deal_description_text ); ?>"
+                  rows="4" cols="50">
+            <?php echo esc_attr( $deal_description_text ); ?>
+        </textarea>
+    </p>
+
+</div><!-- /.fields-meta-box -->
--- dailydeals/custom-metabox-dailydeals.php
+++ dailydeals/custom-metabox-dailydeals.php
@@ -3,20 +3,20 @@
  * Register the meta box
  */
 function register_dailydeals_meta_boxes() {
-    add_meta_box('dailydeals_data_metabox', 
-                 __( 'Daily Deals Data'), 
-                 'dailydeals_display_callback', 
-                 'dailydeals','normal', 'high'
+    add_meta_box( 'dailydeals_data_metabox', 
+                   __( 'Daily Deals Data'), 
+                  'dailydeals_display_callback', 
+                  'dailydeals','normal', 'high'
     );
 }
 
-add_action('add_meta_boxes', 'register_dailydeals_meta_boxes');
+add_action( 'add_meta_boxes', 'register_dailydeals_meta_boxes' );
 
 /**
  * Meta box display callback
  * @param WP_Post $post Current post object
  */
-function dailydeals_display_callback($post) {
+function dailydeals_display_callback( $post ) {
     include 'custom-fields-dailydeals-form.php';
     wp_nonce_field( basename( __FILE__ ), 'dailydeals_meta_box_nonce' );
 }
@@ -26,39 +26,40 @@ function dailydeals_display_callback($post) {
  * Save the meta box content
  * @param int $post_id Post ID
  */
-function save_dailydeals_meta_box($post_id) {
+function save_dailydeals_meta_box( $post_id ) {
 
 	// Return if it's autosave
-    if (defined('DOING_AUTOSAVE' ) && DOING_AUTOSAVE) {
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE  ) {
     	return;
     }
 
     // Check the user's permissions
-    if (!current_user_can( 'edit_post', $post_id)){
+    if ( !current_user_can( 'edit_post', $post_id ) ){
 		  return;
 	  }
 
 	// Verify meta box nonce
-	if (!isset($_POST['dailydeals_meta_box_nonce']) || !wp_verify_nonce($_POST['dailydeals_meta_box_nonce'], basename( __FILE__ ))) {
+	if  (!isset( $_POST['dailydeals_meta_box_nonce'] ) || !wp_verify_nonce( $_POST['dailydeals_meta_box_nonce'], basename( __FILE__ ) ) ) {
 		return;
 	}
 
 	// Check if it's a revision
-    if ($parent_id = wp_is_post_revision($post_id)) {
+    if ( $parent_id = wp_is_post_revision( $post_id ) ) {
         $post_id = $parent_id;
     }
 
     $fields = [
         'dealTitle',
-        'dealDescription',
+        'dealLeadOff',
+        'dealDescriptionText',
     ];
 
     // Run the update with sanitized $_POST data
-    foreach ($fields as $field) {
-        if (array_key_exists($field, $_POST)) {
-            update_post_meta($post_id, $field, sanitize_text_field($_POST[$field]));
+    foreach ( $fields as $field ) {
+        if ( array_key_exists( $field, $_POST )) {
+            update_post_meta( $post_id, $field, sanitize_text_field( $_POST[$field]) );
         }
      }
 }
 
-add_action('save_post', 'save_dailydeals_meta_box');
+add_action( 'save_post', 'save_dailydeals_meta_box' );
